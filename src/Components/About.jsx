c5c6c96f401f0f7cee91914366ae8da6256b8e7f
Shain Sahagun <95956735+m3mentomor1@users.noreply.github.com>
2024-06-14 00:42:58 +0800

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,12 +31,10 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "UI/UX Design",
+  "Web Development",
+  "Cross-platform Mobile Development",
+  "AI/Machine Learning",
 ];
 
 /**
